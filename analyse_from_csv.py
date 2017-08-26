27710f5b9a2b9b1affbc4118bc6a53d6e0b53a2a
JamesBradbury <github@jamesbradbury.co.uk>
2017-08-26 08:55:10 +0100

    Show correlation for missed sleep only. Attempt to read correlation from list of dicts (not working yet) Sort and print out results with commas for easy copy-paste.

--- analyse_from_csv.py
+++ analyse_from_csv.py
@@ -5,18 +5,42 @@ Exploratory python/pandas code to look for correlations in sleep data.
 import pandas as pd
 from csv import DictReader
 
-essential_columns = {'Date', 'Exercise (1-5)', 'Hours that night'}
-data_filename = 'local_data/sleepdata_20Aug2017.csv'
+essential_columns = {'Exercise (1-5)', 'Hours that night'}
+data_filename = 'local_data/sleepdata_latest.csv'
 
+input_list_of_dicts = []
 # Test we can read the CSV ok.
 with open(file=data_filename) as csv_file:
     for index, row in enumerate(DictReader(f=csv_file)):
         # print([row[c] for c in row.keys()])
         if all([row[c] for c in essential_columns]):
-            print(index, row)
+            if float(row['Missed sleep']) < float(0.5):
+                print("Rounding down {} for {}".format(row['Missed sleep'], row['Date']))
+                row['Missed sleep'] = 0
+            input_list_of_dicts.append(row)
 
-df = pd.read_csv(data_filename, index_col='Date', parse_dates=True)
-# df['H-L'] = df.High - df.Low
+df = pd.read_csv(data_filename, index_col='Date', parse_dates=True, infer_datetime_format=True)
+# df = pd.DataFrame(input_list_of_dicts)
+# df.set_index('Date')
 
-print(df.describe())
-print(df.corr())
+df.dropna(subset=list(essential_columns), inplace=True)
+#print('Missed sleep:-', df['Missed sleep'])
+
+correlation = df.corr()
+ms = correlation['Missed sleep']
+ms_sorted = ms.sort_values()
+
+for i, j in ms_sorted.iteritems():
+    print(i.strip() + ', ' + str(j).strip())
+print('----')
+print(ms_sorted)
+
+htn = correlation['Hours that night']
+htn_sorted = htn.sort_values()
+print(htn)
+
+
+# names = [n for n in dir(df.corr()) if not n.startswith('_')]
+
+
+
