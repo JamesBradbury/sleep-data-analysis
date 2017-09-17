6f6b918c3f76819a5afab855e753bb309cc280de
JamesBradbury <github@jamesbradbury.co.uk>
2017-09-17 11:34:20 +0100

    Missed sleep scaled and de-noised. Add unit tests to investigate new missed sleep measure. Refactor analysis into a method.

--- analyse_from_csv.py
+++ analyse_from_csv.py
@@ -8,15 +8,23 @@ from csv import DictReader
 essential_columns = {'Exercise (1-5)', 'Hours that night'}
 non_float_columns = {'', 'Date'}
 data_filename = 'local_data/sleepdata_latest.csv'
+target_sleep = float(8)
+hours_noise_threshold = float(0.6)
 
 
-def jb_score(row):
-    target = float(7.5)
-    max_hours_beyond_target = float(row['Max possible (hrs)']) - target
-    # row['Hours that night'] - row['Missed sleep']
-    missed_sleep_under_target = max(target, float(row['Max possible (hrs)'])) - float(row['Missed sleep'])
-    missed_sleep_over_target = float(row['Missed sleep']) - missed_sleep_under_target
-    return float(row['Hours that night'] * 3) - (missed_sleep_under_target * 3) - missed_sleep_over_target
+def missed_sleep_scaled(row):
+    useful_max = min(target_sleep, row['Max possible (hrs)'])
+    if useful_max == float(0):
+        # result is invalid.
+        return -1
+    max_expected_hours = min(target_sleep, row['Max possible (hrs)'])
+    useful_missed_sleep = max_expected_hours - min(row['Hours that night'], target_sleep)
+    if useful_missed_sleep <= hours_noise_threshold:
+        useful_missed_reduced_noise = float(0)
+    else:
+        useful_missed_reduced_noise = useful_missed_sleep
+    print("useful_missed_reduced_noise:", useful_missed_reduced_noise)
+    return float(10) * useful_missed_reduced_noise / useful_max
 
 
 def try_float(s):
@@ -26,48 +34,49 @@ def try_float(s):
         return s
 
 
-input_list_of_dicts = []
-print("Max:, Htn:, JB Score:")
-with open(file=data_filename) as csv_file:
-    for index, csv_row in enumerate(DictReader(f=csv_file)):
-        csv_row = {k: try_float(v) for k, v in csv_row.items()}
-        if all([csv_row[c] for c in essential_columns]):
-            if float(csv_row['Hours that night']) > float(csv_row['Max possible (hrs)']):
-                print("WARNING: Hours {} more than maximum {} for {}".format(csv_row['Hours that night'],
-                                                                             csv_row['Max possible (hrs)'],
-                                                                             csv_row['Date']))
-            print("{}, {}, {}".format(csv_row['Max possible (hrs)'], csv_row['Hours that night'],
-                                      jb_score(csv_row)))
-            if float(csv_row['Max possible (hrs)']) < float(6):
-                continue
-            if float(csv_row['Missed sleep']) < float(0.25):
-                # print("Rounding down {} for {}".format(row['Missed sleep'], row['Date']))
-                csv_row['Missed sleep'] = float(0)
-            input_list_of_dicts.append(csv_row)
+def analyse_sleep():
+    input_list_of_dicts = []
+    print("Max:, Htn:, JB Score:")
+    with open(file=data_filename) as csv_file:
+        for index, csv_row in enumerate(DictReader(f=csv_file)):
+            csv_row = {k: try_float(v) for k, v in csv_row.items()}
+            if all([csv_row[c] for c in essential_columns]):
+                if float(csv_row['Hours that night']) > float(csv_row['Max possible (hrs)']):
+                    print("WARNING: Hours {} more than maximum {} for {}".format(csv_row['Hours that night'],
+                                                                                 csv_row['Max possible (hrs)'],
+                                                                                 csv_row['Date']))
+                print("{}, {}, {}".format(csv_row['Max possible (hrs)'], csv_row['Hours that night'],
+                                          missed_sleep_scaled(csv_row)))
+                if float(csv_row['Max possible (hrs)']) < float(6):
+                    continue
+                if float(csv_row['Missed sleep']) < float(0.25):
+                    # print("Rounding down {} for {}".format(row['Missed sleep'], row['Date']))
+                    csv_row['Missed sleep'] = float(0)
+                input_list_of_dicts.append(csv_row)
 
-df_csv = pd.read_csv(data_filename, parse_dates=True, infer_datetime_format=True)
-print(set(df_csv.columns.values))
-df_from_dict = pd.DataFrame(input_list_of_dicts, dtype=float)
-df_from_dict.set_index('Date')
-print(set(df_from_dict))
+    df_csv = pd.read_csv(data_filename, parse_dates=True, infer_datetime_format=True)
+    print(set(df_csv.columns.values))
+    df_from_dict = pd.DataFrame(input_list_of_dicts, dtype=float)
+    df_from_dict.set_index('Date')
+    print(set(df_from_dict))
 
-df_from_dict.dropna(subset=list(essential_columns), inplace=True)
-print('Missed sleep:-', df_from_dict['Missed sleep'])
+    df_from_dict.dropna(subset=list(essential_columns), inplace=True)
+    print('Missed sleep:-', df_from_dict['Missed sleep'])
 
-correlation = df_from_dict.corr()
-print(list(correlation))
-ms = correlation['Missed sleep']
-ms_sorted = ms.sort_values()
+    correlation = df_from_dict.corr()
+    print(list(correlation))
+    ms = correlation['Missed sleep']
+    ms_sorted = ms.sort_values()
 
-for i, j in ms_sorted.iteritems():
-    print(i.strip() + ', ' + str(j).strip())
-print('----')
-print(ms_sorted)
+    for i, j in ms_sorted.iteritems():
+        print(i.strip() + ', ' + str(j).strip())
+    print('----')
+    print(ms_sorted)
 
-# htn = correlation['Hours that night']
-# htn_sorted = htn.sort_values()
-# print(htn)
-# names = [n for n in dir(df.corr()) if not n.startswith('_')]
+    # htn = correlation['Hours that night']
+    # htn_sorted = htn.sort_values()
+    # print(htn)
+    # names = [n for n in dir(df.corr()) if not n.startswith('_')]
 
 
 
---
+++ tests/test_jb_score.py
@@ -0,0 +1,100 @@
+import unittest
+
+from analyse_from_csv import missed_sleep_scaled
+
+
+class TestMissedSleepScaled(unittest.TestCase):
+
+    def setUp(self):
+        self.row = {}
+
+    def test_max_over_8_ignored(self):
+        self.row['Max possible (hrs)'] = float(8)
+        self.row['Hours that night'] = float(7)
+        result_8 = missed_sleep_scaled(self.row)
+        self.row['Max possible (hrs)'] = float(9.5)
+        result_9 = missed_sleep_scaled(self.row)
+        self.assertEqual(result_8, result_9)
+
+    def test_long_sleep_means_smaller_scaled_missed_sleep_score(self):
+        self.row['Max possible (hrs)'] = float(8)
+        self.row['Hours that night'] = float(7.25)
+        result_missed_0p75_of_8 = missed_sleep_scaled(self.row)
+        self.row['Max possible (hrs)'] = float(7)
+        self.row['Hours that night'] = float(6.25)
+        result_missed_0p75_of_7 = missed_sleep_scaled(self.row)
+        self.row['Max possible (hrs)'] = float(6)
+        self.row['Hours that night'] = float(5.25)
+        result_missed_0p75_of_6 = missed_sleep_scaled(self.row)
+        self.assertGreater(result_missed_0p75_of_6, result_missed_0p75_of_7)
+        self.assertGreater(result_missed_0p75_of_7, result_missed_0p75_of_8)
+
+    def test_more_missed_means_greater_scaled_missed_sleep_score(self):
+        self.row['Max possible (hrs)'] = float(7.5)
+        self.row['Hours that night'] = float(7.25)
+        result_missed_0p75_of_7p5 = missed_sleep_scaled(self.row)
+        self.row['Max possible (hrs)'] = float(7.5)
+        self.row['Hours that night'] = float(6)
+        result_missed_1p5_of_7 = missed_sleep_scaled(self.row)
+        self.row['Max possible (hrs)'] = float(7.5)
+        self.row['Hours that night'] = float(5)
+        result_missed_2p5_of_7 = missed_sleep_scaled(self.row)
+        self.assertGreater(result_missed_2p5_of_7, result_missed_1p5_of_7)
+        self.assertGreater(result_missed_1p5_of_7, result_missed_0p75_of_7p5)
+
+
+class TestExploreSleepScaled(unittest.TestCase):
+    def setUp(self):
+        self.row = {}
+
+    def test_missed_1_max_8(self):
+        self.row['Max possible (hrs)'] = float(8)
+        self.row['Hours that night'] = float(7)
+        result = missed_sleep_scaled(self.row)
+        print(self.row['Max possible (hrs)'], self.row['Hours that night'], result)
+
+    def test_missed_1_max_7(self):
+        self.row['Max possible (hrs)'] = float(7)
+        self.row['Hours that night'] = float(6)
+        result = missed_sleep_scaled(self.row)
+        print(self.row['Max possible (hrs)'], self.row['Hours that night'], result)
+
+    def test_missed_1_max_6(self):
+        self.row['Max possible (hrs)'] = float(6)
+        self.row['Hours that night'] = float(5)
+        result = missed_sleep_scaled(self.row)
+        print(self.row['Max possible (hrs)'], self.row['Hours that night'], result)
+
+    def test_missed_2_max_8(self):
+        self.row['Max possible (hrs)'] = float(8)
+        self.row['Hours that night'] = float(6)
+        result = missed_sleep_scaled(self.row)
+        print(self.row['Max possible (hrs)'], self.row['Hours that night'], result)
+
+    def test_missed_2_max_7(self):
+        self.row['Max possible (hrs)'] = float(7)
+        self.row['Hours that night'] = float(5)
+        result = missed_sleep_scaled(self.row)
+        print(self.row['Max possible (hrs)'], self.row['Hours that night'], result)
+
+    def test_missed_2_max_6(self):
+        self.row['Max possible (hrs)'] = float(6)
+        self.row['Hours that night'] = float(4)
+        result = missed_sleep_scaled(self.row)
+        print(self.row['Max possible (hrs)'], self.row['Hours that night'], result)
+
+    def test_missed_0p75_max_6p5(self):
+        self.row['Max possible (hrs)'] = float(6.5)
+        self.row['Hours that night'] = float(5.75)
+        result = missed_sleep_scaled(self.row)
+        print(self.row['Max possible (hrs)'], self.row['Hours that night'], result)
+
+    def test_missed_0p75_max_6(self):
+        self.row['Max possible (hrs)'] = float(5)
+        self.row['Hours that night'] = float(4.25)
+        result = missed_sleep_scaled(self.row)
+        print(self.row['Max possible (hrs)'], self.row['Hours that night'], result)
+
+
+if __name__ == '__main__':
+    unittest.main()
